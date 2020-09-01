c968e0beb87d959c70d9730661a258aec993a781
ian038 <ianphua3@gmail.com>
2020-09-01 13:56:04 -0400

    Refactored landing page and resume section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { makeStyles } from '@material-ui/core/styles';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import { AppBar, Toolbar, Button, IconButton, Typography } from '@material-ui/core'
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Drawer, IconButton, Typography, Divider, List, ListItem, ListItemText } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
     menuButton: {
       marginRight: theme.spacing(2),
     },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        })
+      },
+      appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
     title: {
       flexGrow: 1,
     },
@@ -21,18 +36,45 @@ const useStyles = makeStyles((theme) => ({
         marginRight: 30,
         textDecoration: 'none',
         color: 'white'
-    }
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+      drawerPaper: {
+        width: drawerWidth,
+      },
+      drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+      }
   }));
 
 
 function App() {
     const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+  
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
 
   return (
     <div className="demo-big-content">
-        <AppBar position="static" className="header-color">
+        <AppBar position="fixed" id="header-color" className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}>
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerOpen}>
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" className={classes.title}>
@@ -52,28 +94,44 @@ function App() {
                 </Typography>
             </Toolbar>
         </AppBar>
-    {/* <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/work">Work</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/work">Work</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
+        <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+            paper: classes.drawerPaper,
+        }}>
+            <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+            </div>
+            <Divider />
+            <List>
+                <ListItem button>
+                    <ListItemText>
+                        <Link className="drawer_links" to="/resume">Resume</Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem button>
+                    <ListItemText>
+                        <Link className="drawer_links" to="/aboutme">About Me</Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem button>
+                    <ListItemText>
+                        <Link className="drawer_links" to="/work">Work</Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem button>
+                    <ListItemText>
+                        <Link className="drawer_links" to="/contact">Contact</Link>
+                    </ListItemText>
+                </ListItem>
+            </List>
         </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout> */}
+        <Main />
 </div>
   );
 }
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,69 +1,82 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React from 'react';
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 import Education from './education';
 import Experience from './experience';
 
-
-class About extends Component {
-    render() {
-        return (
-            <div>
-                <Grid>
-                    <Cell col={4}>
-                        <div style={{ textAlign: 'center' }}>
-                            <img
-                                src="https://cdn2.iconfinder.com/data/icons/teen-people-face-avatar-6/500/teen_109-512.png"
-                                alt="avatar"
-                                style={{ height: '200px' }}
-                            />
-                        </div>
-                        <h2 style={{ paddingTop: '1em' }}>Chai Ian Phua</h2>
-                        <h4 style={{ color: 'gray' }}>Software Engineer</h4>
-                        <hr style={{ borderTop: '3px solid #833fb2', width: '95%' }} />
-                        <p>Hello, pleased to meet you and welcome to my portfolio!</p>
-                        <hr style={{ borderTop: '3px solid #833fb2', width: '95%' }} />
-                        <h5>Phone</h5>
-                        <p>(614) 607-0882</p>
-                        <h5>Email</h5>
-                        <p>ianphua3@gmail.com</p>
-                        <h5>Web</h5>
-                        <p>my-portfolio-96172.firebaseapp.com</p>
-                        <hr style={{ borderTop: '3px solid #833fb2', width: '95%' }} />
-                    </Cell>
-                    <Cell className="resume-right-col" col={8}>
-                        <h2>Education</h2>
-                        <Education
-                            start="Jan 2016"
-                            end="Aug 2018"
-                            schoolName="The Ohio State University"
-                            schoolDescription="Transfered from Sunway University to attend The Ohio State University. Majored in Psychology and eventually graduated with a Bachelor of Arts in August 2018."
-                        />
-                        <Education
-                            start="Jan 2014"
-                            end="Dec 2015"
-                            schoolName="Sunway University"
-                            schoolDescription="Was accepted into the American Transfer Program at Sunway University in home country of Malaysia. After completing the first two years of the program in Malaysia, transfered to The Ohio State University in January 2016."
-                        />
-                        <hr style={{ borderTop: '3px solid #e22947' }} />
-                        <h2>Experience</h2>
-                        <Experience
-                            start="Jan 2019"
-                            end="Current"
-                            jobName="Software Engineer"
-                            jobDescription="Worked as a freelancing software engineer. Built multiple voice apps for Amazon Alexa and Google Actions. Made extensive use of Firebase to store customer data of clients. Also designed and built out webpages and websites using Javascript front end libraries such as React and Vue for UI/UX."
-                        />
-                        <Experience
-                            start="Oct 2018"
-                            end="Jan 2019"
-                            jobName="Aide"
-                            jobDescription="Hired as an aide at Haugland Learning Center. Learnt to practice Behavioral Psychology using techniques to record behavior data and to use recorded data to customize an educational plan for children with Autism Spectrum Disorder."
-                        />
-                        <hr style={{ borderTop: '3px solid #e22947' }} />
-                    </Cell>
-                </Grid>
-            </div>
-        );
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
     }
+  }));
+
+export default function About() {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.root}>
+          <Grid container spacing={3} direction="row">
+            <Grid item style={{ marginLeft: 40 }} xs={3}>
+            <div style={{ textAlign: 'center' }}>
+                <img
+                src="https://cdn2.iconfinder.com/data/icons/teen-people-face-avatar-6/500/teen_109-512.png"
+                alt="avatar"
+                style={{ height: '200px', marginTop: 100 }}
+                />
+            </div>
+            <h2 style={{ paddingTop: '1em' }}>Chai Ian Phua</h2>
+            <h4 style={{ color: 'gray' }}>Software Engineer</h4>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '95%' }} />
+            <p>Hello, pleased to meet you and welcome to my portfolio!</p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '95%' }} />
+            <h5>Phone</h5>
+            <p>(614) 607-0882</p>
+            <h5>Email</h5>
+            <p>ianphua3@gmail.com</p>
+            <h5>Web</h5>
+            <p>my-portfolio-96172.firebaseapp.com</p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '95%' }} />
+            </Grid>
+            <Grid item xs={8} style={{ marginTop: 95, marginLeft: 50 }} className="resume-right-col">
+            <h2>Education</h2>
+            <Education
+            start="Jan 2020"
+            end="April 2022"
+            schoolName="Cincinnati State Technical and Community College"
+            schoolDescription="Currently pursuing an Associates Degree in Software Engineering Technology. Relevant courses include but are not limited to Java, VB.Net, C, SQL and Business Intelligence."
+            />
+            <Education
+            start="Jan 2016"
+            end="Aug 2018"
+            schoolName="The Ohio State University"
+            schoolDescription="Transfered from Sunway University to attend The Ohio State University. Majored in Psychology and eventually graduated with a Bachelor of Arts in August 2018."
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            <h2>Experience</h2>
+            <Experience
+            start="Jan 2019"
+            end="Feb 2020"
+            jobName="Software Engineer Intern"
+            jobDescription="Worked at Voice First Tech. Built multiple voice apps for Amazon Alexa and Google Actions. Made extensive use of Firebase to store customer data of clients. Also designed and built out webpages and websites using Javascript front end libraries such as React and Vue for UI/UX."
+            />
+            <Experience
+            start="Oct 2018"
+            end="Jan 2019"
+            jobName="Aide"
+            jobDescription="Hired as an aide at Haugland Learning Center. Learnt to practice Behavioral Psychology using techniques to record behavior data and to use recorded data to customize an educational plan for children with Autism Spectrum Disorder."
+            />
+            <Experience
+            start="Feb 2018"
+            end="April 2019"
+            jobName="Student Manager"
+            jobDescription="Delegated tasks to employees to ensure facility's daily goals are met through team work. Assessed and evaluated errors and risks to minimize production downtime."
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            </Grid>
+      </Grid>
+    </div>
+    );
 }
 
-export default About;
+
+
